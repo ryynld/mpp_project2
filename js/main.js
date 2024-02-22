1288ced3d1c69dfbf0e6786312c9b2bdcd960fc9
Reynald M Hena <henareynald@gmail.com>
2024-02-22 12:15:34 +0700

    penyesuaian konten

--- js/main.js
+++ js/main.js
@@ -49,7 +49,7 @@ var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
+    this.classList.toggle("aktif");
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
